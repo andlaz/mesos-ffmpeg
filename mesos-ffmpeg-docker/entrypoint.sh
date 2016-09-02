3f4e0407168add36a33a273c648a07dad8c8a9e3
root <root@master1>
2016-09-02 10:42:45 +0000

    Added hardcoded host and port to the entrypoint script

--- mesos-ffmpeg-docker/entrypoint.sh
+++ mesos-ffmpeg-docker/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo $NC_HOST
 echo $VIDEO_URL
-echo $NC_PORT
-exec ffmpeg -i $VIDEO_URL -f mpegts - | nc -l $NC_HOST $NC_PORT
+exec ffmpeg -i $VIDEO_URL -f mpegts - | nc -l 0.0.0.0 8999
